6d4f48d79189b3682219e8a132e8c4a8bcc8fad0
DotNet14 <naizuddin12@gmail.com>
2025-11-30 00:39:17 +0530

    Initial commit: Angular project with certificate CSS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Request } from './app/request/request';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Request, appConfig)
   .catch((err) => console.error(err));
